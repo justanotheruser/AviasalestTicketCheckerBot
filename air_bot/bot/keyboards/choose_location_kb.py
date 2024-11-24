a7663d2df5f2b03a254801b8a94325f7bd4cae92
Denis Sleptsov <hurrdurrrderp@gmail.com>
2024-11-24 23:47:20 +0300

    fix: Denver-город и Denver-аэропорт

--- air_bot/bot/keyboards/choose_location_kb.py
+++ air_bot/bot/keyboards/choose_location_kb.py
@@ -5,8 +5,12 @@ from air_bot.domain.model import Location
 
 def choose_location_keyboard(locations: list[Location]) -> types.InlineKeyboardMarkup:
     kb = []
+    texts = set()
     for location in locations:
         text = f"{location.name} ({location.code})"
+        if text in texts:
+            continue
+        texts.add(text)
         callback_data = f"{location.name}|{location.code}"
         kb.append([types.InlineKeyboardButton(text=text, callback_data=callback_data)])
     return types.InlineKeyboardMarkup(inline_keyboard=kb)
